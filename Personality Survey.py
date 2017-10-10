dc485a1af9aa221a4599e3397298b6d5e7c93660
moomill46 <mgetz@gcds.net>
2017-10-10 07:52:38 -0400

    First Commit

---
+++ Personality Survey.py
@@ -0,0 +1,13 @@
+print("What is your name?")
+name = input()
+print("Hello " + name)
+print("What is your favorite animal?")
+animal = input()
+if animal == "dog":
+    print("Mine too!")
+elif animal == "elephant":
+    print("That is an awesome animal!")
+else:
+    print("That is a cool animal.")
+    
+    
